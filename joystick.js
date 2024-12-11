5691d9ce21ed33be7d618d41bd5e41e76ed274ab
David espinosa <104380606+davespser@users.noreply.github.com>
2024-12-11 16:31:50 +0100

    Update joystick.js

--- joystick.js
+++ joystick.js
@@ -1,5 +1,5 @@
 // Importamos nipple.js
-import nipplejs from './modulos/nipple.js';  // Asegúrate de que la ruta sea correcta
+import Nipple from './modulos/nipple.js';  // Asegúrate de que la ruta sea correcta
 
 let joystick = null;
 
