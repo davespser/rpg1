deb1914ff161fe35cc1b6e9414c494a693c6d7ab
David espinosa <104380606+davespser@users.noreply.github.com>
2024-12-11 11:06:31 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { crearEscena } from './escena.js';
 
-// Crear la escena, la cámara, el renderizador, el cubo y el motor de físicas
+// Crear la escena, la cámara, el renderizador, el cubo y el mundo de físicas
 const { scene, camera, renderer, cube, world, cubeBody, updatePhysics } = crearEscena();
 
 // Función de animación
