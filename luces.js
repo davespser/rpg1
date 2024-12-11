20d8f64a64967676b72c4fda694bf39b304b1e8c
David espinosa <104380606+davespser@users.noreply.github.com>
2024-12-11 13:48:03 +0100

    Create luces.js

---
+++ luces.js
@@ -0,0 +1,9 @@
+import * as THREE from 'three';
+
+export function crearLuces(scene) {
+    // Luz ambiental
+    const luzAmbiental = new THREE.AmbientLight(0xffffff, 0.5);
+    scene.add(luzAmbiental);
+
+    // Luz direccional
+    const luzDireccional = new THREE.DirectionalLight(0xffffff, 1);
