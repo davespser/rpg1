c21563a9997adcc77b1f8df1b54f0429e9bb7002
David espinosa <104380606+davespser@users.noreply.github.com>
2024-12-12 13:36:13 +0100

    Update cuerpos.js

--- cuerpos.js
+++ cuerpos.js
@@ -3,14 +3,13 @@ import * as CANNON from 'cannon-es';
 export function crearCuboFisico() {
     // Crear un cuerpo físico para el cubo
     const cuboBody = new CANNON.Body({
-        mass: 1,  // Masa del cubo
+        mass: 5,  // Masa del cubo
         position: new CANNON.Vec3(0, 5, 0),  // Posición inicial
         shape: new CANNON.Box(new CANNON.Vec3(0.5, 0.5, 0.5)) // Forma del cubo
     });
 
     // Eliminar o reducir el damping para permitir un movimiento más fluido
-    cuboBody.linearDamping = 0.05; // Reducir damping
-    cuboBody.angularDamping = 0.05; // Reducir damping
+     // Reducir damping
 
     return cuboBody;
 }
