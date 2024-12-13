44d6cd9114d0c30bf77a86b1517dee5a48cacbe8
David espinosa <104380606+davespser@users.noreply.github.com>
2024-12-13 18:05:37 +0100

    Update main.js

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ RAPIER.init().then(() => {
         applyMovement } = setupScene(document.body);
 
     function animate() {
+        console.log('Animating')
         requestAnimationFrame(animate);
         const now = performance.now();
         const deltaTime = (now - lastTime) / 1000; // deltaTime en segundos
