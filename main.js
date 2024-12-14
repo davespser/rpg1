d0a32f671fc2706d62ce4798a33af78ac6ba544a
David espinosa <104380606+davespser@users.noreply.github.com>
2024-12-14 10:05:18 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,13 @@
-import RAPIER from '@dimforge/rapier3d-compat';
-import { setupScene } from './setupScene.js';
+import './style.css';
+import { Simulation } from './src/simulacion.js';
 
-let lastTime = performance.now();
+async function init() {
+  try {
+    const simulation = new Simulation();
+    await simulation.init();
+  } catch (error) {
+    console.error('Failed to start application:', error);
+  }
+}
 
-RAPIER.init().then(async () => {
-    try {
-        const { scene, 
-            camera, 
-            renderer, 
-            controls, 
-            updatePhysics, 
-            applyMovement } = await setupScene(document.body);
-
-        function animate() {
-            console.log('Animating');
-            requestAnimationFrame(animate);
-            const now = performance.now();
-            const deltaTime = (now - lastTime) / 1000; // deltaTime in seconds
-            lastTime = now;
-            
-            updatePhysics(deltaTime); // Pass deltaTime to updatePhysics
-            applyMovement();
-            controls.update();
-            renderer.render(scene, camera);
-        }
-
-        animate();
-    } catch (error) {
-        console.error('Error setting up the scene:', error);
-    }
-});
+init();
