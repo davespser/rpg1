b8ac2b70fe508a8650794410b83294273a6e9d33
David espinosa <104380606+davespser@users.noreply.github.com>
2024-12-17 16:04:27 +0100

    Update objetos.js

--- objetos.js
+++ objetos.js
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 import { GLTFLoader } from "GLTFLoader";
 import RAPIER from '@dimforge/rapier3d-compat';
 
-export function cargarModelo(posX = 1, posY = 15, posZ = 1, rutaModelo = './negro.glb', world) {
+export function cargarModelo(posX = 1, posY = 1, posZ = 1, rutaModelo = './negro.glb', world) {
     return new Promise((resolve, reject) => {
         const loader = new GLTFLoader();
         loader.load(
@@ -12,7 +12,8 @@ export function cargarModelo(posX = 1, posY = 15, posZ = 1, rutaModelo = './negr
                 
                 // Modelo visual de Three.js
                 const objeto = gltf.scene;
-                objeto.scale.set(10, 10, 10);  // Escala ajustada
+                objeto.scale.set(10, 10, 10);
+                onjeto.position.set(1,10,1);// Escala ajustada
                 objeto.traverse((node) => {
                     if (node.isMesh) {
                         node.castShadow = true;
