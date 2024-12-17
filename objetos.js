ac7d097912afa0a97bac3d7a499a127ad12a9b7e
David espinosa <104380606+davespser@users.noreply.github.com>
2024-12-17 12:36:04 +0100

    Update objetos.js

--- objetos.js
+++ objetos.js
@@ -36,7 +36,7 @@ export function cargarModelo(posX = 250, posY = 8, posZ = 250, rutaModelo = './n
                     console.log("Modelo y cuerpo físico listos:", { objeto, body });
 
                     // Visualización del colisionador (cuboide verde)
-                    const colliderGeometry = new THREE.BoxGeometry(22, 22, 22); // Ajusta las dimensiones según tu modelo
+                    const colliderGeometry = new THREE.BoxGeometry(40, 40, 40); // Ajusta las dimensiones según tu modelo
                     const colliderMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
                     const colliderMesh = new THREE.Mesh(colliderGeometry, colliderMaterial);
                     colliderMesh.position.set(posX, posY, posZ); // Posición del colisionador
