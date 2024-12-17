dc92780d1b1340b1fd8a43d913616744b566de2c
David espinosa <104380606+davespser@users.noreply.github.com>
2024-12-17 15:30:41 +0100

    Update objetos.js

--- objetos.js
+++ objetos.js
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 import { GLTFLoader } from "GLTFLoader";
 import RAPIER from '@dimforge/rapier3d-compat';
 
-export function cargarModelo(posX = 250, posY = 8, posZ = 250, rutaModelo = './negro.glb', world) {
+export function cargarModelo(posX = 1, posY = 1, posZ = 1, rutaModelo = './negro.glb', world) {
     return new Promise((resolve, reject) => {
         const loader = new GLTFLoader();
         loader.load(
