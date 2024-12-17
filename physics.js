201969ad79058ca03420e8f26909328de1042e17
David espinosa <104380606+davespser@users.noreply.github.com>
2024-12-17 11:16:21 +0100

    Update physics.js

--- physics.js
+++ physics.js
@@ -2,9 +2,14 @@ import RAPIER from '@dimforge/rapier3d-compat';
 
 let world;
 
+let world;
+
 export async function initPhysics() {
     await RAPIER.init();
     world = new RAPIER.World({ x: 0.0, y: -9.81, z: 0.0 });
+    console.log('world después de creación en initPhysics:', world);
+    return world;
+}
 }
 
 export async function createTerrainRigidBody(terrainMesh) {
