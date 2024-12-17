8713baaaed5edde8ab4840bf8e9b88651601c523
David espinosa <104380606+davespser@users.noreply.github.com>
2024-12-17 19:42:42 +0100

    Update objetos.js

--- objetos.js
+++ objetos.js
@@ -32,7 +32,7 @@ export function cargarModelo(posX = 1, posY = 20, posZ = 1, rutaModelo = './negr
                 const colliderGeometry = new THREE.BoxGeometry(size.x, size.y, size.z);
                 const colliderMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
                 const colliderMesh = new THREE.Mesh(colliderGeometry, colliderMaterial);
-                colliderMesh.position.set(0, size.y / 2, 0); 
+                colliderMesh.position.set(0, -2, 0); 
                 objeto.add(colliderMesh);
 
                 console.log("Modelo y colisionador alineados.");
