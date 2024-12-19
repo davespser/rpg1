cbb84f75efc2ff5a7c2ada56f2b4d8a88d9378ca
David espinosa <104380606+davespser@users.noreply.github.com>
2024-12-19 17:20:54 +0100

    Update main.js

--- main.js
+++ main.js
@@ -55,7 +55,7 @@ async function init() {
         // Cargar modelo con física
         const resultado = await cargarModelo(1, 1, 1, './negro.glb', world, scene, true);
         modelo = resultado.modelo;
-        modelo.scale.set(0.5, 0.5, 0.5);
+        modelo.scale.set(1, 1, 1);
         body = resultado.body;
         collider = resultado.collider;
         scene.add(modelo);
