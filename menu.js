21b3bc865518e33914ddf7d36e7ed31a0162b387
David espinosa <104380606+davespser@users.noreply.github.com>
2024-12-19 19:36:09 +0100

    Update menu.js

--- menu.js
+++ menu.js
@@ -16,28 +16,13 @@ export function crearMenuRadial() {
                 </div>
             `).join('')}
         </div>
-        <button id="toggleMenu">Toggle Menu</button>
     `;
     document.body.insertAdjacentHTML('beforeend', menuHTML);
 
     const toggleMenu = document.getElementById('toggleMenu');
     const menuRadial = document.getElementById('menuRadial');
-    const buttons = document.querySelectorAll('.trapezoid-button');
 
-    // Mostrar/ocultar menú radial
     toggleMenu.addEventListener('click', () => {
         menuRadial.classList.toggle('hidden');
-    });
-
-    // Añadir funcionalidad a cada botón para desplazarse fuera del hexágono
-    buttons.forEach((button, index) => {
-        button.addEventListener('click', () => {
-            button.classList.add('expand');
-
-            // Remover la clase después de un tiempo para permitir reutilizar la animación
-            setTimeout(() => button.classList.remove('expand'), 1000);
-
-            console.log(`Botón ${menuItems[index].label} clickeado`);
-        });
     });
 }
