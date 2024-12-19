71d14eded73cdae196379c1c5246cad70e44ad07
David espinosa <104380606+davespser@users.noreply.github.com>
2024-12-19 09:33:56 +0100

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,7 +1,11 @@
 export function crearMenuEstadisticas() {
     const menuItems = [
-        { id: 'vida', label: 'Vida', value: 100 },
-        { id: 'energia', label: 'Energía', value: 100 }
+        { id: 'estadisticas', label: 'estadisticas', value: 100 },
+        { id: 'configuracion', label: 'configuracion', value: 100 },
+        { id: 'inventario', label: 'inventario', value: 100 },
+        { id: 'habilidades', label: 'habilidades', value: 100 },
+        { id: 'color', label: 'color', value: 100 },
+        { id: 'mapa', label: 'mapa', value: 100 }
     ];
 
     const totalItems = menuItems.length;
