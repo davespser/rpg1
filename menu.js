2ee8fac4273a4bb2aa9a039c0a96af322955a66a
David espinosa <104380606+davespser@users.noreply.github.com>
2024-12-19 09:57:09 +0100

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,11 +1,7 @@
 export function crearMenuEstadisticas() {
     const menuItems = [
-        { id: 'estadisticas', label: 'estadisticas', value: 100 },
-        { id: 'configuracion', label: 'configuracion', value: 100 },
-        { id: 'inventario', label: 'inventario', value: 100 },
-        { id: 'habilidades', label: 'habilidades', value: 100 },
-        { id: 'color', label: 'color', value: 100 },
-        { id: 'mapa', label: 'mapa', value: 100 }
+        { id: 'vida', label: 'Vida', value: 100 },
+        { id: 'energia', label: 'Energía', value: 100 }
     ];
 
     const totalItems = 6; // Seis trapezoides en total
