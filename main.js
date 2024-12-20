70c1d20afb7bd7e08c5fbfde1aff5f6b9718f541
David espinosa <104380606+davespser@users.noreply.github.com>
2024-12-20 08:46:28 +0100

    Update main.js

--- main.js
+++ main.js
@@ -57,8 +57,8 @@ async function init() {
 // Puedes pasar las coordenadas y rotación como objetos
         const plane = createPlane(
         { x: -240, y: 0, z: 40 },  // Posición del plano
-        { x: 0, y: 0, z: 0},// rotacion del plano
-        { x: 10, y: 10, z: 50}
+        { x: -90, y: 0, z: 0},// rotacion del plano
+        { x: 50, y: 10, z: 10}
       );
           scene.add(plane);
         // Cargar modelo con física
