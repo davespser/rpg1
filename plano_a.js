5745b60f1936d7f4bf1fe913b6fd3255a5e681f6
David espinosa <104380606+davespser@users.noreply.github.com>
2024-12-20 12:21:59 +0100

    Update plano_a.js

--- plano_a.js
+++ plano_a.js
@@ -97,8 +97,12 @@ function createWaterGeometry(geometry) {
 }
 
 // Función para modificar la geometría con ruido suave para césped
-function createGrassGeometry(geometry) { const positions = geometry.attributes.position.array; for (let i = 0; i < positions.length; i += 3) { positions[i + 2] += Math.random() * 0.3; // z } geometry.computeVertexNormals(); return geometry;
-  
+function createGrassGeometry(geometry) {
+  const positions = geometry.attributes.position.array;
+   for (let i = 0; i < positions.length; i += 3) {
+     positions[i + 2] += Math.random() * 0.3;
+     // z } geometry.computeVertexNormals(); return geometry;
+   }
   // Recalcula las normales de los vértices para reflejar los cambios en la geometría
   geometry.computeVertexNormals();
   
