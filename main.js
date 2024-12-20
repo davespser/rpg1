8a9f4841cb0a6c5bc787cd40f3ce45091b25fc47
David espinosa <104380606+davespser@users.noreply.github.com>
2024-12-20 12:33:57 +0100

    Update main.js

--- main.js
+++ main.js
@@ -58,8 +58,8 @@ async function init() {
         const cube = createCube(
         { x: -240, y: 3, z: 80 },  // Posición del plano
         { x: 0, y: 0, z: 0},// rotacion del plano
-        { x: 0.5, y: 25, z: 670},
-        { x: 600, y: 50, z: 600 }
+        { x: 1, y: 25, z: 670},
+        { x: 100, y: 400, z: 900 }
       );
           scene.add(cube);
         // Cargar modelo con física
