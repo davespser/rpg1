65b97d3970f5d4271ca2d8482b6387c4a36a7d5d
David espinosa <104380606+davespser@users.noreply.github.com>
2024-12-20 20:06:15 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,35 +2,22 @@ import * as THREE from 'three';
 import { initScene } from './scene.js';
 import { crearMenuRadial } from './menu.js';
 import { Stats } from './stats.js';
-import { cargarModelo } from './objetos.js';
-import { addBuildings } from './entorno_f.js';
 import { createSky } from './sky.js';
 
-let modelo;
 const { scene, camera, renderer, controls } = initScene();
 const stats = new Stats();
 crearMenuRadial();
 createSky(scene);
-
-const texturePath = 'https://raw.githubusercontent.com/davespser/rpg1/main/casa_t.jpg';
-const heightMapPath = 'https://raw.githubusercontent.com/davespser/rpg1/main/casa.png';
 
 /**
  * Función principal de inicialización
  */
 async function init() {
     try {
-        // Cargar modelo
-        const resultado = await cargarModelo(1, 1, 1, './negro.glb', scene, true);
-        modelo = resultado.modelo;
-        scene.add(modelo);
-
-        console.log("Modelo añadido a la escena.");
-
-        // Configuración de la cámara
+        // Configurar la cámara
         camera.position.set(250, 10, 300);
-        camera.lookAt(modelo.position);
-        controls.target.copy(modelo.position);
+        camera.lookAt(0, 0, 0);
+        controls.target.set(0, 0, 0);
         controls.update();
 
         // Iniciar animación
@@ -46,11 +33,6 @@ async function init() {
  */
 function animate() {
     requestAnimationFrame(animate);
-
-    // Sincronizar modelo con cuerpo físico (aquí ya no es necesario)
-    if (modelo) {
-        // Actualizar modelo si se necesita animar
-    }
 
     // Renderizar la escena
     controls.update();
